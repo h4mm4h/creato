08bad7814ad9dd1273db254953f7126ba09247f0
h4mm4h <fr3quent@gmail.com>
2018-02-08 14:34:25 +0200

    gallery update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-;(function() {
+;(function($) {
 	"use strict";
  var menuToggle = document.querySelector('.ba-menu-toggle'),
  		overlay= document.querySelector('.ba-overlay'),
@@ -12,4 +12,21 @@
     });
 
 
-})();
+    var worksGrid = $('.ba-works-grid');
+window.onload = function () {
+	$(worksGrid).isotope({
+		// options
+		itemSelector: '.ba-grid-item',
+		percentPosition: true,
+		masonry:{
+			columnWidth: '.grid-sizer'
+		}
+		// layoutMode: 'fitRows'
+
+	});
+	$('.ba-works__menu').on( 'click', 'button', function() {
+		var filterValue = $(this).attr('data-filter');
+		worksGrid.isotope({ filter: filterValue });
+	});
+};
+})(jQuery);
